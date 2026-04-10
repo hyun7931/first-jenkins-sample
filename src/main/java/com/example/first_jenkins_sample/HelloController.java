004e6fd2239d48fd31a0dc822b51afc33aa5f144
hyun7931 <shlee4185@gmail.com>
2026-04-10 11:12:19 +0900

    Feat: 11:12

--- src/main/java/com/example/first_jenkins_sample/HelloController.java
+++ src/main/java/com/example/first_jenkins_sample/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Jenkins CI/CD 11:10";
+        return "Hello, Jenkins CI/CD 11:12";
     }
 }
