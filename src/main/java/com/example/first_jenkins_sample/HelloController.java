4aa413085817bda3a2a8d2863cc7ea1e3954b06d
hyun7931 <shlee4185@gmail.com>
2026-04-10 11:44:46 +0900

    11:44

--- src/main/java/com/example/first_jenkins_sample/HelloController.java
+++ src/main/java/com/example/first_jenkins_sample/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Jenkins CI/CD 11:27";
+        return "Hello, Jenkins CI/CD 11:44";
     }
 }
